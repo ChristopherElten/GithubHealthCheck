a6b430542b7687fc5436a6587c44276460cd1fa8
ChristopherElten <Chris.Elten@gmail.com>
2018-12-01 13:11:52 -0500

    init api requests to get commits from repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,77 @@
 import { Component, OnInit } from '@angular/core';
-import { GithubService } from './github.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { mergeMap, flatMap, map, tap } from 'rxjs/operators';
+import { of, from } from 'rxjs';
 
+import { GithubService } from './github.service';
+import { token } from './../../secret';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // TODO - Move constants to config/env file
-  apiBaseUrl: string;
-  listCommitsOnRepoApiUrl: string;
-  getCommitApiUrl: string;
-
   title = 'app';
+  owner = 'angular';
+  repo = 'angular';
 
   constructor(private githubService: GithubService, private http: HttpClient) { }
 
   ngOnInit() {
-    // Make request for latest 30 Commits on repo
-    // https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
-    
-    // This is a placeholder get request to demo http get
-    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(t => console.log(t));
-
-    // Make request for each commit
-    // https://developer.github.com/v3/repos/commits/#get-a-single-commit
-
-
+    this.http.get(
+      getCommitsOnRepoApiUrl(this.owner, this.repo),
+      getHttpOptions(token)
+    )
+    .pipe(
+      // Flatten arraylist of commits
+      flatMap((commits: any) =>
+       of(...commits)
+      ),
+      // Make request for each commit information
+      mergeMap((commit) =>
+        this.http.get(
+          getCommitApiUrl(this.owner, this.repo, commit.sha),
+          getHttpOptions(token)
+        )
+      )
+    )
+    .subscribe((res: any) => {
+      // Do something with files here
+      console.log(res);
+    });
   }
+}
+
+  // TODO - Move constants to config/env file
+const apiBaseUrl = 'https://api.github.com';
+
+function getCommitsOnRepoApiUrl(owner: string, repo: string): string {
+  // GET /repos/:owner/:repo/commits
+  // https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+  return `${apiBaseUrl}/repos/${owner}/${repo}/commits?per_page=2`;
+}
+
+function getCommitApiUrl(owner: string, repo: string, sha: string): string {
+  // GET /repos/:owner/:repo/commits/:sha
+  // https://developer.github.com/v3/repos/commits/#get-a-single-commit
+  return `${apiBaseUrl}/repos/${owner}/${repo}/commits/${sha}`;
+}
+
+function getHttpOptions(TOKEN: string): {
+  headers?: HttpHeaders | {
+      [header: string]: string | string[];
+  };
+  observe?: 'body';
+  params?: HttpParams | {
+      [param: string]: string | string[];
+  };
+  reportProgress?: boolean;
+  responseType?: 'json';
+  withCredentials?: boolean;
+} {
+  return {
+    headers: {
+      Authorization: `token ${TOKEN}`
+    }
+  };
 }
